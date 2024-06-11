b8301f07cba648675dd56818005fc6d662133979
Nigarglm <tu7mk3dbf@code.edu.az>
2024-06-11 11:09:26 +0400

    Task has done

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-let button = document.querySelectorAll('.iconBtn');
-let backdrop = document.querySelectorAll('.backdrop');
-let modalEl = document.querySelectorAll('.modalEl');
+const openModalBtn = document.querySelectorAll('.openModalBtn');
+const modal = document.querySelectorAll('.modal');
+const backdrop = document.querySelectorAll('.backdrop');
+const closeModalBtn = document.querySelectorAll('.closeModalBtn');
+const passwordElement = document.querySelectorAll('.password');
+const closeBtn = document.querySelectorAll('.closeBtn');
+const saveBtn = document.querySelectorAll('.saveBtn');
+const toggleTypeBtn = document.querySelectorAll('.toggleTypeBtn');
+
+
+const openModal = () => {
+    modal.classList.remove("hidden");
+    backdrop.classList.remove("hidden");
+}
+
+const closeModal = () => {
+    modal.classList.add("hidden");
+    backdrop.classList.add("hidden");
+}
+
+openModalBtn.addEventListener("click",openModal);
+
+backdrop.addEventListener("click",closeModal);
+closeModalBtn.addEventListener("click",closeModal);
+closeBtn.addEventListener("click",closeModal);
+saveBtn.addEventListener("click",closeModal);
+
+
+toggleTypeBtn.addEventListener("click", () => {
+    passwordElement.type = passwordElement.type === "text" ? "password" : "text";
+    eyeIcon.classList = passwordElement.type === "text"
+    ? "fa-regular fa-eye"
+    : "fa-regular fa-eye-slash";
+})
