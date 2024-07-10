335f35df7a18d310ef3c7b3e01a8fa818009e095
Nigarglm <tu7mk3dbf@code.edu.az>
2024-07-10 10:41:17 +0400

    To do List has  done

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-const openModalBtn = document.getElementById("openModalBtn");
-const modalBtn = document.getElementById("modal");
-const backdrop = document.getElementById("backdrop");
-const closeModalBtn = document.getElementById("closeModalBtn");
-const toggleTypeBtn = document.getElementById("toggleTypeBtn");
-const eyeIcon = document.querySelector("#toggleTypeBtn i");
+const addBtn = document.getElementById("add");
 
-const closeBtn = document.getElementById("closeBtn");
-const saveBtn = document.getElementById("saveBtn");
-const passwordElement = document.getElementById("password");
+const taskListContainer = document.querySelector(".taskList");
+const taskList = document.querySelector(".taskList ul");
+const addInput = document.getElementById("input");
 
-
-const openModal = () => {
-    modal.classList.remove("hidden");
-    backdrop.classList.remove("hidden");
-};
-
-
-const closeModal = () => {
-    modal.classList.add("hidden");
-    backdrop.classList.add("hidden");
-};
-
-openModalBtn.addEventListener("click", openModal);
-
-const closeElements = [backdrop, closeModalBtn, closeBtn, saveBtn];    
-
-closeElements.forEach((element) => {
-    element.addEventListener("click", closeModal);
+const tasks = [];         
+addBtn.addEventListener ("click", (event) => {
+    event.preventDefault();                        
+   const givenText = addInput.value;          
+   if (givenText === "") {               
+    return;
+   } 
+  
+    tasks.push(givenText);           
+    displayTasks ();
+    addInput.value = "";        
 });
 
-toggleTypeBtn.addEventListener("click", () => {
-    if (passwordElement.type === "password") {
-        passwordElement.type = "text";
-        eyeIcon.classList.remove("fa-eye-slash");
-        eyeIcon.classList.add("fa-eye");
-    } else {
-        passwordElement.type = "password";
-        eyeIcon.classList.remove("fa-eye");
-        eyeIcon.classList.add("fa-eye-slash");
-    }
-});
+const displayTasks = () => {
+    taskList.innerHTML = "";         
+    tasks.forEach((task) => {
+        taskList.innerHTML += `<li>            
+        <span>${task}</span>                             
+        <button class="btn delete">Delete Task</button>     
+        </li`;
+    });
+
+    const deleteBtns = document.querySelectorAll(".delete");
+    deleteBtns.forEach((deleteBtn, index) => {
+    deleteBtn.addEventListener("click", () => {
+    deleteTask (index);             
+    });
+    });
+};
+
+   const deleteTask = (index) => {
+    tasks.splice (index, 1);
+    displayTasks();          
+   };
+
+  
